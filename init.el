9b3458bade54282d53681827c051fcdc02a81b9a
vhqr0 <zq_cmd@163.com>
2022-09-10 17:15:24 +0800

    +python-skip-set-ipython

--- init.el
+++ init.el
@@ -179,8 +179,11 @@
 (with-eval-after-load 'cc-mode
   (define-key c-mode-base-map (kbd "C-c C-h") 'cc-help))
 
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "--simple-prompt")
+(defvar +python-skip-set-ipython nil)
+
+(unless +python-skip-set-ipython
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "--simple-prompt"))
 
 (setq js-indent-level 2)
 
