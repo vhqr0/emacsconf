18fdb4a744d99f70d647a79acf93a96edd25abda
vhqr0 <zq_cmd@163.com>
2022-10-06 21:24:20 +0800

    org-capture-templates update

--- misc/org-capture-templates.el
+++ misc/org-capture-templates.el
@@ -1,7 +1,5 @@
 (setq org-capture-templates
-      '(("i" "inbox" entry (file "inbox.org") "* %?\n  %u\n  %a" :clock-keep t)
+      '(("i" "inbox" entry (file "inbox.org") "* %u %?\n  %a" :clock-keep t)
         ("t" "todo" entry (file "todo.org") "* TODO %^t %?" :clock-keep t)
-        ("p" "payment" table-line (file "payment.org") "|%u|%?||" :clock-keep t)
-        ("w" "working" entry (file+headline "sched.org" "sched") "* %u %? :working:" :clock-in t :clock-resume t)
-        ("r" "reading" entry (file+headline "sched.org" "sched") "* %u %? :reading:" :clock-in t :clock-resume t)
-        ("c" "coding" entry (file+headline "sched.org" "sched") "* %u %? :coding:" :clock-in t :clock-resume t)))
+        ("p" "pay" table-line (file "pay.org") "|%u|%?||" :clock-keep t)
+        ("c" "clock" entry (file+headline "clock.org" "clock") "* %u %? :%^g:" :clock-in t :clock-resume t)))
