83a7c7da22a441676d9400a6460e5403c7b9001f
vhqr0 <zq_cmd@163.com>
2022-11-09 23:17:07 +0800

    visual-regexp

--- lib/+autoload.el
+++ lib/+autoload.el
@@ -200,6 +200,28 @@ Rename symbol at point on all its occurrences." t nil)
 
 ;;;***
 
+;;;### (autoloads nil "visual-regexp" "visual-regexp.el" (0 0 0 0))
+;;; Generated autoloads from visual-regexp.el
+
+(autoload 'vr/mc-mark "visual-regexp" "\
+Convert regexp selection to multiple cursors.
+
+\(fn REGEXP START END)" t nil)
+
+(autoload 'vr/replace "visual-regexp" "\
+Regexp-replace with live visual feedback.
+
+\(fn REGEXP REPLACE START END)" t nil)
+
+(autoload 'vr/query-replace "visual-regexp" "\
+Use `vr/query-replace' like you would use `query-replace-regexp'.
+
+\(fn REGEXP REPLACE START END)" t nil)
+
+(register-definition-prefixes "visual-regexp" '("vr--" "vr/"))
+
+;;;***
+
 (provide '+autoload)
 ;; Local Variables:
 ;; version-control: never
