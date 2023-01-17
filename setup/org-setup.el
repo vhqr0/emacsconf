2c143a9e9ad2f60bab5add749cc7532299af5a1a
vhqr0 <zq_cmd@163.com>
2023-01-17 11:14:10 +0800

    evil-org-agenda, update org setup

--- setup/org-setup.el
+++ setup/org-setup.el
@@ -9,8 +9,7 @@
       org-default-notes-file (expand-file-name "notes.org" org-directory))
 
 (setq org-special-ctrl-a/e t
-      evil-org-key-theme
-      '(navigation return textobjects shift calendar))
+      evil-org-key-theme '(navigation return textobjects calendar))
 
 (define-key ctl-x-l-map "oa" 'org-agenda)
 (define-key ctl-x-l-map "oc" 'org-capture)
@@ -24,7 +23,10 @@
 (add-hook 'org-mode-hook 'evil-org-mode)
 
 (with-eval-after-load 'org
-  (add-to-list 'org-modules 'org-tempo))
+  (add-to-list 'org-modules 'org-tempo)
+
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 
 
@@ -32,11 +34,13 @@
 
 (setq org-roam-directory (expand-file-name "org-roam" user-emacs-directory))
 
+(setq org-roam-node-display-template
+      (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+
 (define-key ctl-x-l-map "nt" 'org-roam-buffer-toggle)
 (define-key ctl-x-l-map "nf" 'org-roam-node-find)
 (define-key ctl-x-l-map "ni" 'org-roam-node-insert)
 (define-key ctl-x-l-map "nc" 'org-roam-capture)
-(define-key ctl-x-l-map "nj" 'org-roam-dailies-capture-today)
 
 (with-eval-after-load 'org-roam
   (org-roam-db-autosync-mode 1))        ; Notice: load org-roam before edit roam files
