1d4cba423f445af34c1da7de31d1389004861659
vhqr0 <zq_cmd@163.com>
2023-01-19 01:10:57 +0800

    w32-setup: set coding system for `grep`

--- misc/w32-setup.el
+++ misc/w32-setup.el
@@ -5,6 +5,10 @@
 (setq process-coding-system-alist
       '(("[rR][gG]" . (utf-8-dos . gbk-dos)))) ; rg: input is gbk and output is utf-8 :-)
 
+(defun +w32-grep-set-coding-system ()   ; `grep' doesn't match `process-coding-system-alist'
+  (setq-local default-process-coding-system '(utf-8-dos . gbk-dos)))
+(advice-add 'grep-process-setup :after '+w32-grep-set-coding-system)
+
 ;; for git asking password
 (setenv "GIT_ASKPASS" "git-gui--askpass")
 (setenv "SSH_ASKPASS" "git-gui--askpass")
