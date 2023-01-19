9cf7a6b82aaa3951987c496d10693c908de74ea3
vhqr0 <zq_cmd@163.com>
2023-01-19 12:17:45 +0800

    w32 use wsl sdcv

--- misc/w32-setup.el
+++ misc/w32-setup.el
@@ -1,3 +1,7 @@
+
+
+;;* coding system setup
+
 (prefer-coding-system 'utf-8)
 
 (setq default-process-coding-system '(gbk-dos . gbk-dos)) ; cmd, clip, ...: gbk
@@ -9,10 +13,26 @@
   (setq-local default-process-coding-system '(utf-8-dos . gbk-dos)))
 (advice-add 'grep-process-setup :after '+w32-grep-set-coding-system)
 
+(defmacro +w32-with-process-coding-system-utf-8 (&rest body)
+  `(let ((default-process-coding-system '(utf-8-dos . gbk-dos)))
+     ,@body))
+
+(defun +w32-around-proocess-coding-system-utf-8 (func &rest args)
+  (+w32-with-process-coding-system-utf-8
+   (apply func args)))
+
+
+
+;;* program setup
+
 ;; for git asking password
 (setenv "GIT_ASKPASS" "git-gui--askpass")
 (setenv "SSH_ASKPASS" "git-gui--askpass")
 
 ;; assume using unix tools shipped with git: C:\Program Files\Git\usr\bin
 ;; fix find resolve to Windows find, rename git find to find2
-(defvar find-program "find2")
+(setq find-program "find2")
+
+;; use wsl sdcv
+(setq sdcv-program "wsl sdcv")
+(advice-add 'sdcv :around '+w32-around-proocess-coding-system-utf-8)
