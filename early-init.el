9f24d55572d82c6fbc29696e996d58d8e992b0c8
vhqr0 <zq_cmd@163.com>
2023-01-26 20:00:01 +0800

    early-init: inhibit native-comp warnings, add gc and read process buffer, etc

--- early-init.el
+++ early-init.el
@@ -2,7 +2,8 @@
 
 ;;* elisp compile
 (setq load-prefer-newer t
-      byte-compile-docstring-max-column 65536)
+      byte-compile-docstring-max-column 65536
+      native-comp-async-report-warnings-errors 'silent)
 
 ;;* inhibit file name handler alist while init
 (let ((alist file-name-handler-alist))
@@ -10,10 +11,13 @@
   (add-hook 'after-init-hook
             (lambda () (setq file-name-handler-alist alist))))
 
-;;* inhibit gc while init
+;;* gc: inhibit while init, increase after init
 (setq gc-cons-percentage 0.6
       gc-cons-threshold most-positive-fixnum)
 (add-hook 'after-init-hook
           (lambda ()
             (setq gc-cons-percentage 0.1
-                  gc-cons-threshold 2000000)))
+                  gc-cons-threshold 100000000))) ; ~100M
+
+;;* increase read process buffer
+(setq read-process-output-max (* 1024 1024)) ; 1M
