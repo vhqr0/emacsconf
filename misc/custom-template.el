39ffd39c3a591538dde0c74c37c70325b486aa0d
vhqr0 <zq_cmd@163.com>
2023-03-08 22:36:59 +0800

    clear custom template misc setup lists

--- misc/custom-template.el
+++ misc/custom-template.el
@@ -1,5 +1,5 @@
 (add-to-list '+package 'ef-themes)
 (add-hook 'after-init-hook 'ef-themes-load-random)
 
-(dolist (setup '("fonts" "latex" "pyim"))
+(dolist (setup '("fonts" "pyim"))
   (load-file (expand-file-name (concat setup "-setup.el") +misc-directory)))
