5210d4dee7aa33a58b43a75c8441efc1043b33fc
vhqr0 <zq_cmd@163.com>
2023-04-21 11:19:51 +0800

    consult-line-at-point, customize consult: preview, marginalia align, etc

--- setup/consult-setup.el
+++ setup/consult-setup.el
@@ -3,6 +3,7 @@
 
 
 ;;* vertico
+(setq marginalia-align 'right)
 
 (vertico-mode 1)
 (marginalia-mode 1)
@@ -18,24 +19,24 @@
 (with-eval-after-load 'evil-collection-vertico
   (when evil-collection-setup-minibuffer
     (evil-collection-define-key 'normal 'vertico-map
-      "\C-u" 'vertico-scroll-down
-      "\C-d" 'vertico-scroll-up
-      "gg"   'vertico-first
-      "G"    'vertico-last)))
+      "gg" 'vertico-first
+      "G"  'vertico-last)))
 
 ;;* consult
 
 (setq completion-in-region-function 'consult-completion-in-region)
 
 ;;** search
+(defalias 'consult-line-at-point 'consult-line)
+
 (define-key search-map "s" 'consult-line)
 (define-key search-map "S" 'consult-ripgrep)
+(global-set-key "\C-s" 'consult-line-at-point)
 
-;;** switch
+;;** buffer
 (global-set-key [remap switch-to-buffer] 'consult-buffer)
 (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
 (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
-(global-set-key [remap bookmark-jump] 'consult-bookmark)
 
 ;;** nav
 (global-set-key [remap goto-line] 'consult-goto-line)
@@ -51,3 +52,14 @@
 
 ;;** misc
 (global-set-key [remap load-theme] 'consult-theme)
+
+;;** preview
+
+(with-eval-after-load 'consult
+  (consult-customize
+   consult-line-at-point
+   :initial (thing-at-point 'symbol)
+   consult-buffer consult-buffer-other-window consult-buffer-other-frame
+   :preview-key nil
+   consult-grep consult-git-grep consult-ripgrep consult-theme
+   :preview-key '(:debounce 0.5 any)))
