0ccfd1cce6c0b46c0cdda684964f74276fe18313
vhqr0 <zq_cmd@163.com>
2023-07-20 15:51:59 +0800

    enable web mode engine detection

--- misc/web-setup.el
+++ misc/web-setup.el
@@ -6,4 +6,5 @@
   :init
   (setq web-mode-code-indent-offset 2
         web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2))
+        web-mode-css-indent-offset 2
+        web-mode-enable-engine-detection t))
