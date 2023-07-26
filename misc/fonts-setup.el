3a5e1ed6431f202a528138bbbd4e8ffccb59aca7
vhqr0 <zq_cmd@163.com>
2023-07-26 09:50:57 +0800

    update hy-setup, fonts-setup iosevka lxgw

--- misc/fonts-setup.el
+++ misc/fonts-setup.el
@@ -1,16 +1,10 @@
-(defvar +fonts-en
-  (if (eq system-type 'windows-nt)
-      "Cascadia Mono"
-    "DejaVu Sans Mono"))
-(defvar +fonts-cjk
-  (if (eq system-type 'windows-nt)
-      "STXingkai"
-    "WenQuanYi Micro Hei"))
+(defvar +fonts-en "Iosevka")
+(defvar +fonts-cjk "LXGW WenKai Mono")
 
 (let ((font (font-spec :name +fonts-en)))
   (when (find-font font)
     (set-face-attribute 'default nil :font font)))
 
 (let ((font (font-spec :name +fonts-cjk)))
-  (dolist (charset '(cjk-misc han bopomofo kana hangul))
-    (set-fontset-font t charset font)))
+  (dolist (chars '(cjk-misc han bopomofo kana hangul))
+    (set-fontset-font t chars font)))
--- misc/hy-setup.el
+++ misc/hy-setup.el
@@ -3,24 +3,22 @@
   :init
   (setq hy-jedhy--enable? nil
         hy-shell--interpreter-args nil)
+  (autoload 'hy-syntax-propertize-function "hy-mode")
   :config
   (add-hook 'hy-mode-hook 'paredit-mode)
-
-  (with-eval-after-load 'hy-shell
-    (require 'hy-mode))
+  (add-hook 'inferior-hy-mode 'paredit-mode)
 
   (evil-define-key 'normal hy-mode-map "gz" 'run-hy)
 
   (defun hy-shell-macroexpand-current-form ()
     (interactive)
-    (hy-shell--eval-1
-      (format "(hy.macroexpand '%s)" (hy--current-form-string))))
+    (save-selected-window
+      (hy-shell--eval-1
+        (format "(hy.macroexpand '%s)" (hy--current-form-string)))))
   (bind-key "C-c C-m" 'hy-shell-macroexpand-current-form hy-mode-map)
 
   (with-eval-after-load 'evil-eval
     (defun +hy-shell-send-region (start end)
-      (hy-shell--eval-1 (buffer-substring start end)))
-    (add-to-list 'evil-eval-alist '(hy-mode . +hy-shell-send-region)))
-
-  (with-eval-after-load 'eglot
-    (add-to-list 'eglot-server-programs '(hy-mode . ("hyuga")))))
+      (save-selected-window
+        (hy-shell--eval-1 (buffer-substring start end))))
+    (add-to-list 'evil-eval-alist '(hy-mode . +hy-shell-send-region))))
